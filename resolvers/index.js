f7a4541f7204fab683df998a2be32ec952b0a889
D Domingo <ddomingo@atlassian.com>
2024-05-17 13:40:27 +1000

    Delete resolvers directory

--- resolvers/index.js
+++
@@ -1,9 +0,0 @@
-import Resolver from "@forge/resolver";
-
-const resolver = new Resolver();
-
-// TODO: Add resolvers to register and query for nicknames, for example:
-// resolver.define('registerNickname', (req) => {
-// });
-
-export const handler = resolver.getDefinitions();
